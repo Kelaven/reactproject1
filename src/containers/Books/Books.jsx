09d324ae84ce802926cd521476596e8598e8cf4d
Kelaven <kevin.lavenant@live.fr>
2024-03-22 17:39:13 +0100

    CRUD OK

--- src/containers/Books/Books.jsx
+++ src/containers/Books/Books.jsx
@@ -7,14 +7,15 @@ import UpdateForm from "./UpdateForm/UpdateForm.jsx";
 const Books = (props) => {
 
 
-    // ! Variable
+    // ! Variables
     let [allBooks, setAllBooks] = useState([
         { id: 1, title: "Les Ombres de Shadwell", author: "Lucie Arnault", pages: 352 },
         { id: 2, title: "Au-delà des Cimes", author: "Julien Moreau", pages: 289 },
         { id: 3, title: "Chroniques du Dernier Horizon", author: "Émilie Dubois", pages: 467 },
         { id: 4, title: "L'Écho des Âmes Perdues", author: "Marc Lefèvre", pages: 523 }
     ]);
-    const [updateBookId, setUpdateBookId] = useState(null);
+
+    const [updateBookId, setUpdateBookId] = useState(null); // null est la valeur initiale de l'état updateBookId. Cela signifie qu'au début, updateBookId n'a pas de valeur spécifique et est considéré comme non défini ou "pas en mode mise à jour".
 
 
     // ! Fonctions
@@ -37,7 +38,7 @@ const Books = (props) => {
     }
 
     const updateBookHandler = (updateBookId) => {
-        setUpdateBookId(updateBookId);
+        setUpdateBookId(updateBookId); // on met à jour un livre
     }
 
     const validateUpdateBookHandler = (id, title, author, pages) => {
@@ -50,11 +51,8 @@ const Books = (props) => {
         })
         setAllBooks(updatedBooks); // on met à jour l'état avec la nouvelle liste de livres
         setUpdateBookId(null); // on réinitialise updateBookId pour quitter le mode mise à jour
-        // console.log(id);
-        // console.log(title);
-        // console.log(author);
-        // console.log(pages);
-    }
+        // updateBookId est utilisé pour déterminer quel livre est actuellement en cours de mise à jour. 
+        // Lorsque l'on appelle setUpdateBookId(null), on réinitialise l'état updateBookId à null, ce qui indique que plus aucun livre n'est sélectionné pour la mise à jour.
 
 
     return (
